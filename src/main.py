69c45ba8ed0731f9c0ae56acb83628b4ec7102b3
Berlet Maxim <berletmaximq@gmail.com>
2026-03-21 18:37:45 +0300

    started work

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# var 2
+OPERATOR = np.array([
+    [  333/11,   288/11,   18/11,  270/11],
+    [  819/22,  1305/22, -315/11,  324/11],
+    [-1257/44, -1773/44,  453/22, -315/11],
+    [  -81/44,  -549/44,  351/22,  108/11]
+])
+
+
+def main() -> None:
+    # l1 norm
+    print(np.linalg.norm(OPERATOR, ord=1))
+    
+if __name__ == "__main__":
+    main()
